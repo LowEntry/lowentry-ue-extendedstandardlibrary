6be96bd42d71c0b5ed338dbca12d5e0bbd87cf4e
Patrick Vane <patrick_vane@lowentry.com>
2016-06-25 13:12:28 +0200

    Fix.

--- Source/LowEntryExtendedStandardLibrary/Public/Classes/LowEntryExtendedStandardLibrary.h
+++ Source/LowEntryExtendedStandardLibrary/Public/Classes/LowEntryExtendedStandardLibrary.h
@@ -227,13 +227,13 @@ public:
 	* Returns the Team ID of the given Actor.
 	*/
 	UFUNCTION(BlueprintPure, Category = "Low Entry|Extended Standard Library|Utilities|AI", Meta = (DisplayName = "Get Generic Team ID"))
-		void GetGenericTeamId(AActor* Target, uint8& TeamId);
+		static void GetGenericTeamId(AActor* Target, uint8& TeamId);
 	
 	/**
 	* Sets the Team ID of the given Actor.
 	*/
 	UFUNCTION(BlueprintCallable, Category = "Low Entry|Extended Standard Library|Utilities|AI", Meta = (DisplayName = "Set Generic Team ID"))
-		void SetGenericTeamId(AActor* Target, const uint8 TeamId);
+		static void SetGenericTeamId(AActor* Target, const uint8 TeamId);
 
 
 
