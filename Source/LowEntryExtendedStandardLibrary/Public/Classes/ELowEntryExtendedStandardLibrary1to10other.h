0bce3e6e881e47ff6d873353c16fa383d7740ade
Patrick Vane <patrick_vane@lowentry.com>
2020-06-30 20:02:00 +0200

    Fix.

--- Source/LowEntryExtendedStandardLibrary/Public/Classes/ELowEntryExtendedStandardLibrary1to10other.h
+++ Source/LowEntryExtendedStandardLibrary/Public/Classes/ELowEntryExtendedStandardLibrary1to10other.h
@@ -11,15 +11,15 @@
 UENUM(BlueprintType)
 enum class ELowEntryExtendedStandardLibrary1to10other : uint8
 {
-	_1_ = 0,
-	_2_ = 1,
-	_3_ = 2,
-	_4_ = 3,
-	_5_ = 4,
-	_6_ = 5,
-	_7_ = 6,
-	_8_ = 7,
-	_9_ = 8,
-	_10_ = 9,
-	Other = 10,
+	_1_ = 1,
+	_2_ = 2,
+	_3_ = 3,
+	_4_ = 4,
+	_5_ = 5,
+	_6_ = 6,
+	_7_ = 7,
+	_8_ = 8,
+	_9_ = 9,
+	_10_ = 10,
+	Other = 0,
 };
