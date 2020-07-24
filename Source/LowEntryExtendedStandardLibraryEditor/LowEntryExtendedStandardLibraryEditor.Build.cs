e2c533511c85ff8fe6fb0496e187eeb78c8e1a09
Patrick Vane <patrick_vane@lowentry.com>
2020-07-24 14:23:10 +0200

    Attempting to fix an error message.

--- Source/LowEntryExtendedStandardLibraryEditor/LowEntryExtendedStandardLibraryEditor.Build.cs
+++ Source/LowEntryExtendedStandardLibraryEditor/LowEntryExtendedStandardLibraryEditor.Build.cs
@@ -1,56 +1,54 @@
+using UnrealBuildTool;
 using System.IO;
 
-namespace UnrealBuildTool.Rules
+public class LowEntryExtendedStandardLibraryEditor : ModuleRules
 {
-	public class LowEntryExtendedStandardLibraryEditor : ModuleRules
-	{
-		public LowEntryExtendedStandardLibraryEditor(ReadOnlyTargetRules Target) : base(Target)
-        {
-            PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	public LowEntryExtendedStandardLibraryEditor(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-            PublicIncludePaths.AddRange(
-				new string[] {
-                    Path.Combine(ModuleDirectory, "Public/Module"),
-                    Path.Combine(ModuleDirectory, "Public/Classes"),
-				}
-			);
+        PublicIncludePaths.AddRange(
+			new string[] {
+                Path.Combine(ModuleDirectory, "Public/Module"),
+                Path.Combine(ModuleDirectory, "Public/Classes"),
+			}
+		);
 
-			PrivateIncludePaths.AddRange(
-				new string[] {
-                    Path.Combine(ModuleDirectory, "Private/Module"),
-                    Path.Combine(ModuleDirectory, "Private/Classes"),
-				}
-			);
+		PrivateIncludePaths.AddRange(
+			new string[] {
+                Path.Combine(ModuleDirectory, "Private/Module"),
+                Path.Combine(ModuleDirectory, "Private/Classes"),
+			}
+		);
 
-			PublicDependencyModuleNames.AddRange(
-				new string[]
-				{
-					"Engine",
-					"Core",
-					"CoreUObject",
-					"InputCore",
-					"Slate",
-					"EditorStyle",
-					"AIModule",
-					"BlueprintGraph",
-					"LowEntryExtendedStandardLibrary",
-				}
-			);
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Engine",
+				"Core",
+				"CoreUObject",
+				"InputCore",
+				"Slate",
+				"EditorStyle",
+				"AIModule",
+				"BlueprintGraph",
+				"LowEntryExtendedStandardLibrary",
+			}
+		);
 
-			PrivateDependencyModuleNames.AddRange(
-				new string[]
-				{
-					"EditorStyle",
-					"KismetCompiler",
-					"UnrealEd",
-					"GraphEditor",
-					"SlateCore",
-                    "Kismet",
-                    "KismetWidgets",
-                    "PropertyEditor",
-                    "ToolMenus",
-                }
-			);
-		}
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"EditorStyle",
+				"KismetCompiler",
+				"UnrealEd",
+				"GraphEditor",
+				"SlateCore",
+                "Kismet",
+                "KismetWidgets",
+                "PropertyEditor",
+                "ToolMenus",
+            }
+		);
 	}
 }
